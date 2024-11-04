5dbe87f50a819d536fff4192ed33eb79cb3087ca
ThomasStoianov <thomasgstoianov2@gmail.com>
2024-11-04 16:26:32 -0300

    Feat: exercicio 3 feito

---
+++ Assets/scripts/Exercicio03.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Exercicio03 : MonoBehaviour
+{
+    [SerializeField] bool playerVivo; //true
+    [SerializeField] bool powerUp; //true
+    double vidaPlayer = 100;
+    void Start()
+    {
+        if (playerVivo == false && powerUp == true)
+        {
+            vidaPlayer = vidaPlayer * 0.5;
+            print("Ainda não, vida atual " + vidaPlayer);
+        }
+        else
+        {
+            print("Game Over");
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
