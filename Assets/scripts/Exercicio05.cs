13e36df07421fd2059c7d71ae70bde5594e7b22d
ThomasStoianov <thomasgstoianov2@gmail.com>
2024-11-04 16:43:16 -0300

    Feat: exercicio 5 feito

---
+++ Assets/scripts/Exercicio05.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+/*
+ (Pontuação de missão) Após completar uma missão, o jogador
+recebe pontos. Verifique se ele ganhou mais de 50 pontos. Se sim,
+exiba "Missão bem-sucedida"; caso contrário, "Missão
+incompleta".*/
+
+public class Exercicio05 : MonoBehaviour
+
+{
+    [SerializeField] float pontosGanhos = 51;
+    void Start()
+    {
+        if (pontosGanhos > 50)
+        {
+            print("Missão Bem-Sucedida");
+        }
+        else
+        {
+            print("Missão Incompleta");
+        }
+    }
+
+  
+    void Update()
+    {
+        
+    }
+}
