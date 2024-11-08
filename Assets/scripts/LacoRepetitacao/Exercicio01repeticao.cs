087fd541f604704117a8321d28965a22eca2220d
ThomasStoianov <thomasgstoianov2@gmail.com>
2024-11-08 16:55:24 -0300

    Feat: exercicio de repeticao 1 feito

---
+++ Assets/scripts/LacoRepetitacao/Exercicio01repeticao.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Exercicio01repeticao : MonoBehaviour
+{
+    /*(Contagem regressiva de tempo) Crie uma contagem
+    regressiva de 5 a 1 e exiba "Início da partida!" ao final.*/
+    void Start()
+    {
+        for(int i = 5; i >= 0; i--)
+        {
+            print(i);
+        }
+
+        print("Inicio da partida");
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
