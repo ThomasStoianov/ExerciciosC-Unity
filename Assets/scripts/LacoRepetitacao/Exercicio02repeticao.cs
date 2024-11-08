a058ead7105ed60a34f7ab7fbea0cb6a29f028f9
ThomasStoianov <thomasgstoianov2@gmail.com>
2024-11-08 17:05:20 -0300

    Feat: exercicio repeticao 02 feito

---
+++ Assets/scripts/LacoRepetitacao/Exercicio02repeticao.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Exercicio02repeticao : MonoBehaviour
+{
+    /*(Multiplicação de dano) Um personagem ataca cinco vezes, e o
+    dano do ataque aumenta em 2 a cada vez. Exiba o dano a cada
+    ataque.*/
+
+    int dano = 0;
+    int i = 0;
+    void Start()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            dano += 2;
+            print(dano);
+        }
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
