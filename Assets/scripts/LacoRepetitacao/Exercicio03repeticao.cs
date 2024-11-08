1b3e3334e68e13fc1fef4ece09a5b7d2718c74ab
ThomasStoianov <thomasgstoianov2@gmail.com>
2024-11-08 17:21:04 -0300

    Feat: exercicio repeticao 03 feito

---
+++ Assets/scripts/LacoRepetitacao/Exercicio03repeticao.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Exercicio03repeticao : MonoBehaviour
+{
+    /*(Cálculo de moedas coletadas) O jogador coleta 3 moedas a
+    cada fase. Após 10 fases, exiba o total de moedas coletadas.*/
+
+    [SerializeField] int moeda = 3;
+   
+    void Start()
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            moeda += 3;
+        }
+        
+
+        print(moeda);
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
